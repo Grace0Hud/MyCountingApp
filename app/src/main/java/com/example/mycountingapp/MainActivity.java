2aede4296edb008cf76f08af7ad4583939f44420
Grace0Hud <54286731+Grace0Hud@users.noreply.github.com>
2020-09-28 13:41:17 -0700

    count up

--- app/src/main/java/com/example/mycountingapp/MainActivity.java
+++ app/src/main/java/com/example/mycountingapp/MainActivity.java
@@ -1,15 +1,19 @@
 package com.example.mycountingapp;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
 {
+    int num1 = 0;
     //oncreate is the driver method basically - like main
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -27,16 +31,35 @@ public class MainActivity extends AppCompatActivity
 
     public void onClickFirstButton(View view)
     {
-        Toast.makeText(this, "This is the first button!", Toast.LENGTH_SHORT).show();
+        num1++;
+        TextView introTV = (TextView) findViewById(R.id.introTV);
+        introTV.setText("You now have " + num1 + " fishies");
+        //Toast.makeText(this, "This is the first button!", Toast.LENGTH_SHORT).show();
     }
     public void onClickSecondButton(View view)
     {
-        Toast.makeText(this, "This is the second button!", Toast.LENGTH_SHORT).show();
+        //Toast.makeText(this, "This is the second button!", Toast.LENGTH_SHORT).show();
     }
     public void onClickGoToScreen2(View view)
     {
         Intent intent = new Intent(this, Screen2.class);
         startActivity(intent);
     }
+
+    /*
+    public void openDialogue(View view)
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Confirm reset count");
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int i)
+            {
+              //Toast.makeText(this, "Reset confirmed",Toast.LENGTH_LONG).show();
+            }
+        });
+
+     */
+
 
 }//end class
