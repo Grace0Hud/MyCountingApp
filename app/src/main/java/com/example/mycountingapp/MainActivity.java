6d83cf584e2e986d4870e965c38a9f7bcf505088
Grace0Hud <54286731+Grace0Hud@users.noreply.github.com>
2020-09-28 13:09:12 -0700

    Initial commit

---
+++ app/src/main/java/com/example/mycountingapp/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.mycountingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity
+{
+    //oncreate is the driver method basically - like main
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.i("info", "------------This is a message in the log-----------");
+        Log.e("error", "-----------oho that's an error----------");
+        Log.w("warning", "---------You want to be careful with that---------");
+        Log.wtf("What a Terrible Failure", "-----------THAT ONE IS REALLY BAD------------");
+
+        Toast.makeText(this, "Your toast is ready!", Toast.LENGTH_LONG).show();
+    }//end on create
+
+    public void onClickFirstButton(View view)
+    {
+        Toast.makeText(this, "This is the first button!", Toast.LENGTH_SHORT).show();
+    }
+    public void onClickSecondButton(View view)
+    {
+        Toast.makeText(this, "This is the second button!", Toast.LENGTH_SHORT).show();
+    }
+}
